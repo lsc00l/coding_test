ce6a467ee00abc007e26f479e0c0729f07dc12f3
이시원\siwon <lsiwon922@gmail.com>
2024-03-29 18:14:47 +0900

    수정

--- src/cheatsheet/MakeInput.java
+++ src/cheatsheet/MakeInput.java
@@ -2,15 +2,18 @@ package cheatsheet;
 
 import java.io.*;
 
+/**
+ * 프로그래머스 입력을 복사해서 넣으면 자바에서 쓸 수 있게 만들어줌
+ */
 public class MakeInput {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for(int i=0 ; i< 10 ; i++){
-            String input = input = reader.readLine();
+            String input = reader.readLine();
             input = input.replaceAll("\\[", "\\{").replaceAll("\\]", "\\}");
             String[] inputs = input.split("\t");
 
-            System.out.print("System.out.println(solution(");
+            System.out.print("\nSystem.out.println(solution(");
             for (int j = 0; j < inputs.length - 1; j++) {
                 String value = inputs[j];
 
@@ -28,7 +31,7 @@ public class MakeInput {
                     System.out.print(", ");
                 }
             }
-            System.out.println("));\t//" + inputs[inputs.length - 1]);
+            System.out.print("));\t//" + inputs[inputs.length - 1]);
         }
     }
 }
