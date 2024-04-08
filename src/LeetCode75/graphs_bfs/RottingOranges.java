d474311845473e5a3cacfd11a716a81e6be57145
이시원\siwon <lsiwon922@gmail.com>
2024-04-08 15:34:28 +0900

    graphs_bfs solved

--- src/LeetCode75/graphs_bfs/RottingOranges.java
+++ src/LeetCode75/graphs_bfs/RottingOranges.java
@@ -7,10 +7,9 @@ public class RottingOranges {
         System.out.println(r.orangesRotting(new int[][]{{2,1,1},{1,1,0},{0,1,1}})); //4
         System.out.println(r.orangesRotting(new int[][]{{2,1,1},{0,1,1},{1,0,1}})); //-1
         System.out.println(r.orangesRotting(new int[][]{{0,2}}));//0
+        System.out.println(r.orangesRotting(new int[][]{{1,2}}));//2
+        System.out.println(r.orangesRotting(new int[][]{{2},{2},{1},{0},{1},{2},{2}}));//1
     }
-
-
-
 
     int[][] map;
     public int orangesRotting(int[][] grid) {
@@ -26,29 +25,34 @@ public class RottingOranges {
         }
         if(!fresh) return 0;
 
-        return bfs(1, true, 2);
+        return bfs(1, false, 2);
     }
 
 
     public int bfs(int cnt, boolean fresh, int rot) {
-        if(cnt > map.length* map[0].length) return -1;
         for(int i=1 ; i<map.length -1 ; i++) {
             for(int j=1 ; j < map[0].length -1 ; j++){
                 if(map[i][j] == rot){
-                    if(map[i-1][j] > 0) map[i-1][j] = rot + 2;
-                    if(map[i+1][j] > 0) map[i+1][j] = rot + 2;
-                    if(map[i][j-1] > 0) map[i][j-1] = rot + 2;
-                    if(map[i][j+1] > 0) map[i][j+1] = rot + 2;
+                    if(map[i-1][j] > 0 && map[i-1][j] != rot) map[i-1][j] = rot + 2;
+                    if(map[i+1][j] > 0 && map[i+1][j] != rot) map[i+1][j] = rot + 2;
+                    if(map[i][j-1] > 0 && map[i][j-1] != rot) map[i][j-1] = rot + 2;
+                    if(map[i][j+1] > 0 && map[i][j+1] != rot) map[i][j+1] = rot + 2;
 
-                } else if (map[i][j] == 1){
+                }
+            }
+        }
+        for(int i=1 ; i<map.length -1 ; i++) {
+            for (int j = 1; j < map[0].length - 1; j++) {
+                if (map[i][j] == 1){
                     fresh = true;
                 }
             }
         }
-        if(fresh) {
-            int result = bfs(cnt, false, rot+2);
-            cnt = (result == -1) ? -1 : cnt +result;
 
+        if(fresh) {
+            if(cnt > map.length* map[0].length) return -1;
+            int result = bfs(cnt+1, false, rot+2);
+            cnt = (result == -1) ? -1 : result;
         }
 
         return cnt;
