c68d0a5f0e84ab54985338e4d197bb9dee16a37d
이시원\siwon <lsiwon922@gmail.com>
2024-06-11 15:33:13 +0900

    Node 공통클래스로

--- src/LeetCode75/binarytree_dfs/LeafSimilar.java
+++ src/LeetCode75/binarytree_dfs/LeafSimilar.java
@@ -1,24 +1,10 @@
 package LeetCode75.binarytree_dfs;
 
-import java.time.temporal.Temporal;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class LeafSimilar {
 
-    private static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
     public static void main(String[] args) {
         LeafSimilar l = new LeafSimilar();
         System.out.println( l.leafSimilar(new TreeNode(1, new TreeNode(2), new TreeNode(3)), new TreeNode(1, new TreeNode(3), new TreeNode(2))));
